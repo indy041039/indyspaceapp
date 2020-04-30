70d9c553ccc2a63578b68b4fe469d2687be4a3db
Your NameArdong <you@example.comindythemafia@hotmail.com>
2020-04-30 19:40:45 +0700

    first commit

--- app.py
+++ app.py
@@ -1,79 +1,149 @@
-#Chatbot Tutorial (Before connectiing with Firebase)
-#Import Library
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from flask import Flask, jsonify, render_template, request
 import json
-import os
-from flask import Flask
-from flask import request
-from flask import make_response
+import numpy as np
 
-# Flask
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
 app = Flask(__name__)
-@app.route('/', methods=['POST']) #Using post as a method
 
-def MainFunction():
+lineaccesstoken = '#!/usr/bin/python
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from flask import Flask, jsonify, render_template, request
+import json
+import numpy as np
 
-    #Getting intent from Dailogflow
-    question_from_dailogflow_raw = request.get_json(silent=True, force=True)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 
-    #Call generating_answer function to classify the question
-    answer_from_bot = generating_answer(question_from_dailogflow_raw)
-    
-    #Make a respond back to Dailogflow
-    r = make_response(answer_from_bot)
-    r.headers['Content-Type'] = 'application/json' #Setting Content Type
+app = Flask(__name__)
 
-    return r
+lineaccesstoken = 'sVi63a9na79V/b+yduVI4yExkvFLcsZGHpCNgiDRnJVNdlWG22i5ICYiEfSmyX3o0ES4ZZ268XYaGETDPlSEu6htUND4nMeICcbHDvUoj3JHlLO0ZQBLh26jgoWOOk6moTB3eRp8U0+wBHbt54SZQwdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
 
-def generating_answer(question_from_dailogflow_dict):
+####################### new ########################
+@app.route('/')
+def index():
+    return "Hello World!"
 
-    #Print intent that recived from dialogflow.
-    print(json.dumps(question_from_dailogflow_dict, indent=4 ,ensure_ascii=False))
 
-    #Getting intent name form intent that recived from dialogflow.
-    intent_group_question_str = question_from_dailogflow_dict["queryResult"]["intent"]["displayName"] 
+@app.route('/webhook', methods=['POST'])
+def callback():
+    json_line = request.get_json(force=False,cache=False)
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    no_event = len(decoded['events'])
+    for i in range(no_event):
+        event = decoded['events'][i]
+        event_handle(event)
+    return '',200
 
-    #Select function for answering question
-    if intent_group_question_str == 'กินอะไรดี':
-        answer_str = menu_recormentation()
-    elif intent_group_question_str == 'BMI - Confirmed W and H': 
-        answer_str = BMI_calculation(question_from_dailogflow_dict)
-    else: answer_str = "ผมไม่เข้าใจ คุณต้องการอะไร"
 
-    #Build answer dict 
-    answer_from_bot = {"fulfillmentText": answer_str}
-    
-    #Convert dict to JSON
-    answer_from_bot = json.dumps(answer_from_bot, indent=4) 
-    
-    return answer_from_bot
+def event_handle(event):
+    print(event)
+    try:
+        userId = event['source']['userId']
+    except:
+        print('error cannot get userId')
+        return ''
 
-def menu_recormentation(): #Function for recommending menu
-    menu_name = 'สุกี้แห้ง'
-    answer_function = menu_name + ' สิ น่ากินนะ'
-    return answer_function
+    try:
+        rtoken = event['replyToken']
+    except:
+        print('error cannot get rtoken')
+        return ''
+    try:
+        msgId = event["message"]["id"]
+        msgType = event["message"]["type"]
+    except:
+        print('error cannot get msgID, and msgType')
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+        return ''
 
-def BMI_calculation(respond_dict): #Function for calculating BMI
+    if msgType == "text":
+        msg = str(event["message"]["text"])
+        replyObj = TextSendMessage(text=msg)
+        line_bot_api.reply_message(rtoken, replyObj)
 
-    #Getting Weight and Height
-    weight_kg = float(respond_dict["queryResult"]["outputContexts"][2]["parameters"]["Weight.original"])
-    height_cm = float(respond_dict["queryResult"]["outputContexts"][2]["parameters"]["Height.original"])
-    
-    #Calculating BMI
-    BMI = weight_kg/(height_cm/100)**2
-    if BMI < 18.5 :
-        answer_function = "คุณผอมเกินไปนะ"
-    elif 18.5 <= BMI < 23.0:
-        answer_function = "คุณมีนำ้หนักปกติ"
-    elif 23.0 <= BMI < 25.0:
-        answer_function = "คุณมีนำ้หนักเกิน"
-    elif 25.0 <= BMI < 30:
-        answer_function = "คุณอ้วน"
-    else :
-        answer_function = "คุณอ้วนมาก"
-    return answer_function
+    else:
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+    return ''
 
-#Flask
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
+    app.run(debug=True)'
+line_bot_api = LineBotApi(lineaccesstoken)
+
+####################### new ########################
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/webhook', methods=['POST'])
+def callback():
+    json_line = request.get_json(force=False,cache=False)
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    no_event = len(decoded['events'])
+    for i in range(no_event):
+        event = decoded['events'][i]
+        event_handle(event)
+    return '',200
+
+
+def event_handle(event):
+    print(event)
+    try:
+        userId = event['source']['userId']
+    except:
+        print('error cannot get userId')
+        return ''
+
+    try:
+        rtoken = event['replyToken']
+    except:
+        print('error cannot get rtoken')
+        return ''
+    try:
+        msgId = event["message"]["id"]
+        msgType = event["message"]["type"]
+    except:
+        print('error cannot get msgID, and msgType')
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+        return ''
+
+    if msgType == "text":
+        msg = str(event["message"]["text"])
+        replyObj = TextSendMessage(text=msg)
+        line_bot_api.reply_message(rtoken, replyObj)
+
+    else:
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+    return ''
+
+if __name__ == '__main__':
+    app.run(debug=True)
