3248a6b613a3b9e0a19b98204d38b408e096c0f3
Your NameArdong <you@example.comindythemafia@hotmail.com>
2020-05-09 00:57:58 +0700

    make it better

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+    line_bot_api.broadcast(
+        TextSendMessage(text='THIS IS A BROADCAST MESSAGE'))
 
 if __name__ == "__main__":
     app.run()
