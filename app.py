da1aad2b9911e25cc7ed1ceed68ad66e609b5a37
Your NameArdong <you@example.comindythemafia@hotmail.com>
2020-05-09 17:10:36 +0700

    make it better

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, render_template, request
 import json
 import numpy as np
 import requests
-
+from apscheduler.schedulers.blocking import BlockingScheduler
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
 )
@@ -16,6 +16,7 @@ from linebot import (
 )
 
 app = Flask(__name__)
+sched = BlockingScheduler()
 
 lineaccesstoken = 'CSdgziqlU0NO/Sf3MmAkinm92OOKEUYNwNV9xlzta/Z85ZdUUc6sQ5eHl2aJrCGgmR6nFvtNYhhEOG1kG8B0XxayECT8jqSHdszjg7derd6JKI/fZqVDpA5iv9+qICJxk43PeGbYDoQG3Ph7YVAblAdB04t89/1O/w1cDnyilFU='
 linesecret = '74d75448fe78ae3cf293ab1a8cfce9b0'
@@ -53,6 +54,13 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
     line_bot_api.broadcast(
         TextSendMessage(text='THIS IS A BROADCAST MESSAGE'))
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    line_bot_api.broadcast(
+        TextSendMessage(text='THIS IS A BROADCAST MESSAGE'))
+    print('Broadcast every minute test')
+
 
 if __name__ == "__main__":
     app.run()
