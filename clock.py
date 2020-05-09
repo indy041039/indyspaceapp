c744b77f1e32bbd47eb04cb4959d856b44683459
Your NameArdong <you@example.comindythemafia@hotmail.com>
2020-05-09 20:08:48 +0700

    make it better

---
+++ clock.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from apscheduler.schedulers.blocking import BlockingScheduler
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+lineaccesstoken = 'CSdgziqlU0NO/Sf3MmAkinm92OOKEUYNwNV9xlzta/Z85ZdUUc6sQ5eHl2aJrCGgmR6nFvtNYhhEOG1kG8B0XxayECT8jqSHdszjg7derd6JKI/fZqVDpA5iv9+qICJxk43PeGbYDoQG3Ph7YVAblAdB04t89/1O/w1cDnyilFU='
+linesecret = '74d75448fe78ae3cf293ab1a8cfce9b0'
+line_bot_api = LineBotApi(lineaccesstoken)
+handler = WebhookHandler(linesecret)
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    line_bot_api.broadcast(
+        TextSendMessage(text='THIS IS A BROADCAST MESSAGE'))
+    print('BROADCAST')
+
+sched.start()
