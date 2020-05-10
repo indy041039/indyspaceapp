8c101c361d87dc6b72fa31f79d7783e826218a33
Your NameArdong <you@example.comindythemafia@hotmail.com>
2020-05-10 12:31:15 +0700

    make it better

--- app.py
+++ app.py
@@ -65,19 +65,6 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
     line_bot_api.broadcast(
         TextSendMessage(text='THIS IS A BROADCAST MESSAGE'))
-    carousel_template = CarouselTemplate(columns=[
-                CarouselColumn(text='hoge1', title='fuga1', actions=[
-                URIAction(label='Go to line.me', url='https://line.me'),
-                PostbackAction(label='ping', data='ping')
-                ]),
-                CarouselColumn(text='hoge2', title='fuga2', actions=[
-                PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
-                ]),
-            ])
-    template_message = TemplateSendMessage(
-                alt_text='Carousel alt text', template=carousel_template)
-    line_bot_api.reply_message(event.reply_token, template_message)
 
 
 if __name__ == "__main__":
